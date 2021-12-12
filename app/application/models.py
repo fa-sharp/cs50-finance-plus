63f77d6efc10c5dcd4efd282e46c2a19dda8b05e
fa-sharp <farshadpiano@gmail.com>
2021-12-12 07:34:54 -0600

    🗄️ Added transaction table to models

--- app/application/models.py
+++ app/application/models.py
@@ -1,18 +1,33 @@
+from sqlalchemy.ext.hybrid import hybrid_property
 from . import db
-from sqlalchemy import Integer, String, Text, Float, ForeignKey
-
+from sqlalchemy import Integer, String, Text, Numeric, ForeignKey, DateTime
+from sqlalchemy.sql.functions import current_timestamp
+from sqlalchemy.orm import relationship
 
 class User(db.Model):
+
+    def __init__(self, username: str, hash: str, cash: float):
+        self.username=username
+        self.hash=hash
+        self.cash=cash
+
     id = db.Column(Integer, primary_key=True, nullable=False)
 
     username = db.Column(String(80), unique=True, nullable=False)
 
     hash = db.Column(Text(), nullable=False)
 
-    cash = db.Column(Float(precision=3), nullable=False)
+    cash = db.Column(Numeric(scale=4, precision=25), nullable=False)
 
-    portfolio = db.relationship(
-        'Stock', backref='user', lazy=True, cascade="all, delete", passive_deletes=True)
+    portfolio = relationship(
+        'Stock', backref='user', lazy=True, cascade="all, delete", passive_deletes=True, order_by="Stock.symbol")
+    
+    transactions = relationship(
+        'Transaction', backref='user', lazy=True, cascade="all, delete", passive_deletes=True)
+
+    # @hybrid_property
+    # def cash_basis(self):
+    #     return sum(tx.price for tx in self.transactions)
 
     def __repr__(self):
         return '<User %i: %r>' % self.id % self.username
@@ -27,6 +42,35 @@ class Stock(db.Model):
 
     user_id = db.Column(Integer, ForeignKey(
         'user.id', ondelete="CASCADE"), nullable=False)
+    
+    basis_transactions = db.relationship(
+        'Transaction', lazy=True, passive_deletes=True)
 
     def __repr__(self):
         return '<Stock %i: %i shares of %r>' % self.id % self.shares % self.symbol
+
+class Transaction(db.Model):
+
+    def __init__(self, symbol: str, shares: int, price: float):
+        self.symbol = symbol
+        self.shares = shares
+        self.price = price
+
+    id = db.Column(Integer, primary_key=True, nullable=False)
+
+    symbol = db.Column(String(10), nullable=False)
+
+    shares = db.Column(Integer(), nullable=False)
+
+    price = db.Column(Numeric(scale=4, precision=20), nullable=False)
+
+    timestamp = db.Column(DateTime(), nullable=False, server_default=current_timestamp())
+
+    user_id = db.Column(Integer, ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
+    
+    stock_id = db.Column(Integer, ForeignKey(
+        'stock.id', ondelete="SET NULL"), nullable=True)
+
+    def __repr__(self):
+        return '<Tx %i: %i shares of %r at %f>' % self.id % self.shares % self.symbol % self.price
