1776ab1f14ecea679868f62a0d94422a657d45a3
fa-sharp <farshadpiano@gmail.com>
2021-12-13 03:50:24 -0600

    🐛 Fixed bug with Jinja filters - Filters now ignore None/undefined values

--- app/application/jinja_filters.py
+++ app/application/jinja_filters.py
@@ -1,26 +1,40 @@
+from decimal import Decimal
 from jinja2 import Markup
+
 
 def commas(value):
     """Add commas to a numeric value"""
-    return f"{value:,}"
+    try:
+        return f"{value:,}"
+    except:
+        return value
 
 
 def usd(value):
     """Format value as USD."""
-    return f"${value:,.2f}"
+    try:
+        return f"${value:,.2f}"
+    except:
+        return value
 
 
 def cash_flow(value):
     """Format cash flow (highlighting positive or negative amount)"""
-    if value < 0:
-        return Markup(f"<span class='negative'>${value:,.2f}</span>")
-    else:
-        return Markup(f"<span class='positive'>${value:,.2f}</span>")
+    try:
+        if value < 0:
+            return Markup(f"<span class='negative'>${value:,.2f}</span>")
+        else:
+            return Markup(f"<span class='positive'>${value:,.2f}</span>")
+    except:
+        return value
 
 
 def percent(value):
     """Format percentage (highlighting positive/negative)"""
-    if value < 0:
-        return Markup(f"<span class='negative'>{value:.2%}</span>")
-    else:
-        return Markup(f"<span class='positive'>+{value:.2%}</span>")
+    try:
+        if value < 0:
+            return Markup(f"<span class='negative'>{value:.2%}</span>")
+        else:
+            return Markup(f"<span class='positive'>+{value:.2%}</span>")
+    except:
+        return value
