d653d07e875622c3810d9034e01488b5e9c8a337
fa-sharp <farshadpiano@gmail.com>
2021-12-15 04:12:55 -0600

    🔑 Improvements to registration - Add an initial database check for existing username - Welcome message displayed after registration

--- app/application/routes/auth.py
+++ app/application/routes/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, session, redirect
+from flask import Blueprint, render_template, request, session, redirect, current_app, flash
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from application import db
@@ -12,52 +12,60 @@ auth_pages = Blueprint('auth_pages', __name__)
 @auth_pages.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
+    
+    INITIAL_DEPOSIT = 10000 # The initial deposit for all users
 
-    if request.method == "POST":
-
-        username = request.form.get("username")
-        password = request.form.get("password")
-
-        # Ensure username was submitted
-        if not username:
-            return apology("must provide username", 400)
-
-        # Ensure password was submitted
-        if not password:
-            return apology("must provide password", 400)
-
-        # Check if password is valid
-        valid, password_message = check_valid_password(password)
-        if not valid:
-            return apology(password_message, 400)
-
-        # Ensure password == confirmation
-        if not password == request.form.get("confirmation"):
-            return apology("passwords must match", 400)
-
-        hashPassword = generate_password_hash(password)
-        INITIAL_DEPOSIT = 10000
-
-        try:
-            # Create new user and initial deposit transaction
-            new_user = User(username=username,
-                            hash=hashPassword, cash=INITIAL_DEPOSIT)
-            db.session.add(new_user)
-            db.session.flush()
-
-            initial_deposit = Transaction("", 0, INITIAL_DEPOSIT, new_user.id)
-            db.session.add(initial_deposit)
-            
-            db.session.commit()
-            
-        except:
-            db.session.rollback()
-            return apology("username already exists!", 400)
-
-        return render_template("login.html")
-
-    else:
+    # GET: display registration page
+    if request.method == "GET":
         return render_template("register.html")
+
+    # POST: Register a new user
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    # Ensure username was submitted
+    if not username:
+        return apology("must provide username", 400)
+    
+    # Check if username already exists
+    user_exists = db.session.query(
+        User.query.filter(User.username == username).exists()).scalar()
+    if user_exists:
+        return apology("username already exists!", 400)
+
+    # Ensure password was submitted
+    if not password:
+        return apology("must provide password", 400)
+
+    # Check if password is valid
+    valid, password_message = check_valid_password(password)
+    if not valid:
+        return apology(password_message, 400)
+
+    # Ensure password == confirmation
+    if not password == request.form.get("confirmation"):
+        return apology("passwords must match", 400)
+
+    # If all is good, hash the password and create a new user!
+    hash_password = generate_password_hash(password)
+    try:
+        new_user = User(username=username,
+                        hash=hash_password, cash=INITIAL_DEPOSIT)
+        db.session.add(new_user)
+        db.session.flush() # Flush in order to generate the new user id
+
+        initial_deposit = Transaction("", 0, INITIAL_DEPOSIT, new_user.id)
+        db.session.add(initial_deposit)
+        
+        db.session.commit()
+    except:
+        db.session.rollback()
+        current_app.logger.exception("Error adding user to DB")
+        return apology("Server error registering user!", 500)
+
+    # If successful, redirect to login page and display a friendly message
+    flash("You're successfully registered! Login and start trading!!")
+    return render_template("login.html")
 
 
 @auth_pages.route("/login", methods=["GET", "POST"])
