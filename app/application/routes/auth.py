42f37038d3bbc1697abde9357074b52b103343cf
fa-sharp <farshadpiano@gmail.com>
2021-12-15 05:38:46 -0600

    🔑 Added a profile page - User can change password and delete their entire profile

--- app/application/routes/auth.py
+++ app/application/routes/auth.py
@@ -3,7 +3,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from application import db
 from application.models import Transaction, User
-from application.utils import apology, check_valid_password
+from application.utils import apology, check_valid_password, login_required
 
 
 auth_pages = Blueprint('auth_pages', __name__)
@@ -47,10 +47,11 @@ def register():
         return apology("passwords must match", 400)
 
     # If all is good, hash the password and create a new user!
-    hash_password = generate_password_hash(password)
     try:
+        hash_password = generate_password_hash(password)
         new_user = User(username=username,
                         hash=hash_password, cash=INITIAL_DEPOSIT)
+
         db.session.add(new_user)
         db.session.flush() # Flush in order to generate the new user id
 
@@ -114,4 +115,87 @@ def logout():
     session.pop("start_balances", None)
 
     # Redirect user to login form
+    return redirect("/")
+
+
+
+@auth_pages.route("/profile")
+@login_required
+def profile():
+    """A profile page where users can change their password and delete their profile"""
+
+    return render_template("profile.html")
+
+
+@auth_pages.route("/profile/changePassword", methods=["POST"])
+@login_required
+def change_password():
+    """Change the user's password"""
+
+    user_id = session.get("user_id")
+    old_password = request.form.get("oldPassword", "")
+    new_password = request.form.get("newPassword", "")
+    confirmation = request.form.get("confirmation", "")
+
+    user: User = User.query.filter(User.id == user_id).first()
+
+    # Ensure old password is correct
+    if not check_password_hash(user.hash, old_password):
+        return apology("Incorrect password!", 400)
+    
+    # Ensure new password is valid
+    valid, password_message = check_valid_password(new_password)
+    if not valid:
+        return apology(password_message, 400)
+    
+    # Ensure new password = confirmation
+    if not new_password == confirmation:
+        return apology("Passwords don't match!", 400)
+
+    # Hash and save the new password
+    try:
+        current_app.logger.warn(f"Changing password of User {user}")
+        hash_password = generate_password_hash(new_password)
+        user.hash = hash_password
+        db.session.commit()
+    except:
+        db.session.rollback()
+        current_app.logger.exception(f"Error changing password of User {user}")
+        return apology("Server error while changing password!", 500)
+    
+    # Return to home page with confirmation message
+    flash("Successfully changed password!")
+    return redirect("/")
+
+
+@auth_pages.route("/profile/deleteProfile", methods=["POST"])
+@login_required
+def delete_user():
+    """Delete user profile and data"""
+
+    confirm_yes = request.form.get("confirmCheck", "")
+    confirm_delete = request.form.get("confirmText", "")
+
+    # Check if user meant to delete their profile
+    if confirm_yes != "yes" or confirm_delete != "DELETE":
+        flash("Oops! Profile not deleted!")
+        return redirect("/profile")
+
+    user_id = session.get("user_id")
+    user: User = User.query.filter(User.id == user_id).first()
+    
+    # Delete all user data in DB
+    try:
+        current_app.logger.warn(f"Deleting User {user}!!")
+        db.session.delete(user)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        current_app.logger.exception(f"Error deleting User {user}")
+        return apology("Server error while deleting user!", 500)
+
+    # Clear session
+    session.pop("user_id", None)
+    session.pop("start_balances", None)
+
     return redirect("/")
