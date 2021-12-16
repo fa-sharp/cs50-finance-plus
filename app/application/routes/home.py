3bbf56cf37b65043edf6651a4ca234f037168f69
fa-sharp <farshadpiano@gmail.com>
2021-12-16 02:32:43 -0600

    💰 Cost basis and gain/loss info for each stock

--- app/application/routes/home.py
+++ app/application/routes/home.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from typing import List
 from flask import Blueprint, render_template, session
 from sqlalchemy import func
 
@@ -18,21 +19,21 @@ def home():
     user = User.query.filter(User.id == session.get("user_id")).first()
 
     # Calculate the initial cash basis for this user (sum of all DEPOSIT transactions)
-    initialCashBasis = Transaction.query.\
+    initial_cash_basis = Transaction.query.\
         with_entities(func.sum(Transaction.price)).\
         filter(Transaction.user_id == user.id, Transaction.shares == 0).\
         scalar()
 
     # Fetch and calculate updated portfolio data
     returned_portfolio, total_market_value, total_gain_loss = get_portfolio_data(
-        user, initialCashBasis)
+        user, initial_cash_basis)
 
     # Pass portfolio, cash, and totals to Jinja
     return render_template("index.html", portfolio=returned_portfolio,
                            cash=user.cash, totalMarketValue=total_market_value, totalGainLoss=total_gain_loss)
 
 
-def get_portfolio_data(user, initialCashBasis):
+def get_portfolio_data(user: User, initial_cash_basis):
     '''Fetch and calculate user's up-to-date portfolio data. Returns a tuple:
     (portfolio with updated data, total market value, total Gain/Loss)'''
 
@@ -51,11 +52,28 @@ def get_portfolio_data(user, initialCashBasis):
         # Add stock value to the total market value calculation
         total_market_value += stock['value']
 
+        # Calculate stock's cost basis and unrealized gain/loss
+        cost_basis = calculate_cost_basis(stock_row.basis_transactions)
+        stock['costBasis'] = cost_basis
+        stock['gainLoss'] = stock['value'] - stock['costBasis']
+
         # Calculate day change (price day change * shares)
         stock['dayChange'] = stockData['priceChange'] * stock['shares']
         returned_portfolio.append(stock)
 
         # Total Gain/loss = Portfolio current value - initial cash basis
-    total_gain_loss = Decimal(total_market_value - initialCashBasis)
+    total_gain_loss = Decimal(total_market_value - initial_cash_basis)
 
     return returned_portfolio, total_market_value, total_gain_loss
+
+
+def calculate_cost_basis(transactions: List[Transaction]):
+    '''Calculate the cost basis of a user's held stock, given the past transactions. For simplicity,
+    assume the LIFO method'''
+
+    cost_basis = Decimal(0)
+
+    for tx in transactions:
+        cost_basis += (tx.price * tx.shares)
+    
+    return cost_basis
